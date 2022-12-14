612afbf13fb4a0d61bb6854062a202d93f003a93
blesk1 <112616213+blesk1@users.noreply.github.com>
2022-12-14 08:34:53 +0500

    Add files via upload

---
+++ StringLibrary/StringCheckClass.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace StringLibrary
+{
+    public class StringCheckClass
+    {
+        /// <summary>
+        /// Возвращение букв в лист
+        /// </summary>
+        /// <param name="textString">
+        /// В качестве параметра передается строка, которая может быть пустой или содержать некоторый текст. Текст может включать в себя произвольные символы.
+        /// </param>
+        /// <returns>
+        /// Возвращается список символов List<char>. Список должен содержать буквы искомого текста и удовлетворять требованиям, указанным выше.
+        /// </returns>
+        public static List<char> GetLetters(string textString)
+        {
+            textString = textString.ToUpper();
+            List<char> list = new List<char>();
+            foreach (char c in textString)
+            {
+                if (!list.Contains(c) && char.IsLetter(c))
+                {
+                    list.Add(c);
+                }
+            }
+            list.Sort();
+            return list;
+        }
+    }
+}
